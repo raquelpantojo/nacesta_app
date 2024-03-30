b3bf08f07125eeaaf097f904ad6e644e6828f249
raquelpantojo <raquel.pantojo.souza@usp.br>
2024-03-30 19:30:10 -0300

    Update tela_inicio.dart

--- lib/tela_inicio.dart
+++ lib/tela_inicio.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
@@ -62,7 +62,6 @@ class _TelaInicioViewState extends State<TelaInicioView> {
                     },
                     child: Text('Criar uma lista'),
                   ),
-                  // ignore: prefer_const_constructors
                   SizedBox(height: 20), // Espaçamento entre os botões
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
