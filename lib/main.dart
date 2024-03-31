c8ae35ed1f0cc500e537d529b43f7f3dde8210a4
raquelpantojo <raquel.pantojo.souza@usp.br>
2024-03-30 21:42:20 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, duplicate_import
 
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:nacesta_app/tela_addproduto.dart';
 import 'package:nacesta_app/tela_esqueceu.dart';
-import 'tela_lista.dart';
 import 'tela_sobre.dart';
 import 'tela_inicio.dart';
 import 'tela_login.dart';
 import 'tela_cadastro.dart';
+import 'TelaCriarLista.dart';
+import 'tela_addproduto.dart';
 
 void main() {
   runApp(
@@ -28,14 +30,16 @@ class MainApp extends StatelessWidget {
       title: 'Na Cesta',
       initialRoute: 'tinicio',
       routes: {
-        'tlista': (context) => TelaListaView(),
+        'tlogin':(context) => TelaLoginView(),
         'tsobre': (context) => TelaSobreView(),
         'tinicio': (context) => TelaInicioView(),
-        'tlogin':(context) => TelaLoginView(),
         'tesqueceu':(context) => telaEsqueceuView(),
         'tcadastro':(context) => TelaCadastroView(),
+        'tcriarlista':(context) =>TelaCriarListaView(),
+        'taddproduto':(context) =>TelaAdicionarProdutoView(),
+   
       },
-      home: TelaListaView(),
+      home: TelaLoginView(),
     );
   }
 }
